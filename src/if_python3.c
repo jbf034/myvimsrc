14d162648ea29181033888bd89c0450e9d418cf8
lilydjwg <lilydjwg@gmail.com>
2011-06-21 02:08:40 +0800

    编码/解码时使用 surrogateescape 错误处理

--- src/if_python3.c
+++ src/if_python3.c
@@ -70,7 +70,7 @@ static void init_structs(void);
 
 #define PyInt Py_ssize_t
 #define PyString_Check(obj) PyUnicode_Check(obj)
-#define PyString_AsBytes(obj) PyUnicode_AsEncodedString(obj, (char *)p_enc, NULL);
+#define PyString_AsBytes(obj) PyUnicode_AsEncodedString(obj, (char *)p_enc, "surrogateescape");
 #define PyString_FreeBytes(obj) Py_XDECREF(bytes)
 #define PyString_AsString(obj) PyBytes_AsString(obj)
 #define PyString_Size(obj) PyBytes_GET_SIZE(bytes)
@@ -661,8 +661,8 @@ DoPy3Command(exarg_T *eap, const char *cmd)
 
     /* PyRun_SimpleString expects a UTF-8 string. Wrong encoding may cause
      * SyntaxError (unicode error). */
-    cmdstr = PyUnicode_Decode(cmd, strlen(cmd), (char *)p_enc, NULL);
-    cmdbytes = PyUnicode_AsEncodedString(cmdstr, "utf-8", NULL);
+    cmdstr = PyUnicode_Decode(cmd, strlen(cmd), (char *)p_enc, "surrogateescape");
+    cmdbytes = PyUnicode_AsEncodedString(cmdstr, "utf-8", "surrogateescape");
     Py_XDECREF(cmdstr);
     PyRun_SimpleString(PyBytes_AsString(cmdbytes));
     Py_XDECREF(cmdbytes);
@@ -1463,7 +1463,7 @@ LineToString(const char *str)
     }
     *p = '\0';
 
-    result = PyUnicode_Decode(tmp, len, (char *)p_enc, NULL);
+    result = PyUnicode_Decode(tmp, len, (char *)p_enc, "surrogateescape");
 
     vim_free(tmp);
     return result;
