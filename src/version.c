3b28a7c3349c677cfc571e58364697268659c26b
依云 <lilydjwg@gmail.com>
2012-02-29 20:55:42 +0800

    upstream update to 7.3.458

--- src/version.c
+++ src/version.c
@@ -715,6 +715,22 @@ static char *(features[]) =
 static int included_patches[] =
 {   /* Add new patch number below this line */
 /**/
+    458,
+/**/
+    457,
+/**/
+    456,
+/**/
+    455,
+/**/
+    454,
+/**/
+    453,
+/**/
+    452,
+/**/
+    451,
+/**/
     450,
 /**/
     449,
